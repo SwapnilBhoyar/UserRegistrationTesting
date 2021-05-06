1d5e5f442573deed9f9f62c723e36b0a3496194c
SwapnilBhoyar <swapnilbhoyar95@gmail.com>
2021-05-07 01:32:17 +0530

    Added last name

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -2,7 +2,7 @@ import java.util.regex.*;
 import java.util.Scanner;
 public class UserRegistration {
     private boolean check;
-    private String firstName;
+    private String firstName, lastName;
     Scanner sc = new Scanner(System.in);
 
     public void checkFirstName() {
@@ -18,8 +18,22 @@ public class UserRegistration {
         }
     }
 
+    public void checkLastName() {
+        System.out.println("enter last name starts with cap and has minimum 3 character : ");
+        lastName = sc.nextLine();
+        check = Pattern.compile("[A-Z]{1}[a-z]{2,}").matcher(lastName).matches();
+        if (!check) {
+            System.out.println(" last name is invalid ");
+            checkLastName();
+        }
+        else
+            System.out.println(" last name is valid ");
+    }
+
     public static void main(String args[]) {
+
         UserRegistration user = new UserRegistration();
         user.checkFirstName();
+        user.checkLastName();
     }
 }
