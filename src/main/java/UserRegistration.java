d11edf902fea0b50975b0fa90bead551f91c0763
SwapnilBhoyar <swapnilbhoyar95@gmail.com>
2021-05-07 01:34:53 +0530

    Added email

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -2,7 +2,7 @@ import java.util.regex.*;
 import java.util.Scanner;
 public class UserRegistration {
     private boolean check;
-    private String firstName, lastName;
+    private String firstName, lastName, email;
     Scanner sc = new Scanner(System.in);
 
     public void checkFirstName() {
@@ -26,8 +26,22 @@ public class UserRegistration {
             System.out.println(" last name is invalid ");
             checkLastName();
         }
-        else
+        else {
             System.out.println(" last name is valid ");
+        }
+    }
+
+    public void checkEmail() {
+        System.out.println("enter a valid email(Eg. abc.xyz@bl.co.in) : ");
+        email = sc.nextLine();
+        check = Pattern.compile("^[a-zA-Z0-9]+(?:\\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\\.[a-zA-Z0-9]{2,}+)*$").matcher(email).matches();
+        if (!check) {
+            System.out.println(" email is invalid ");
+            checkEmail();
+        }
+        else {
+            System.out.println(" email is valid ");
+        }
     }
 
     public static void main(String args[]) {
@@ -35,5 +49,6 @@ public class UserRegistration {
         UserRegistration user = new UserRegistration();
         user.checkFirstName();
         user.checkLastName();
+        user.checkEmail();
     }
 }
